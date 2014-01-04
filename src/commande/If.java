59c2e1d6f32a7fb9cc32307c7478b43a6c48b3ac
Adequin Renaud <adequin.renaud@gmail.com>
2014-01-04 20:51:27 +0100

    Ajout de IF et test Fractal ok

--- src/commande/If.java
+++ src/commande/If.java
@@ -1,13 +1,43 @@
 package commande;
 
+import algo.Bool;
+import algo.Convert;
+import terminal.TableCommande;
+
+import java.util.ArrayList;
+
 public class If extends Commande{
     @Override
     public String execute(String parametres) {
 
 
         String [] argument = getCmdParam(parametres);
-        if (argument.length != 3 || argument.length != 5) return ErrorToString("1", argument[0]);
+        if (argument.length != 3 && argument.length != 5) return ErrorToString("1", argument[0]);
 
+        ArrayList<String> instructions = Convert.valeurStringtArgument(argument[2]);
+
+        Bool bool = new Bool(getPanelDessin().getCurseur());
+        if(bool.doBooleanTest(argument[1]).equalsIgnoreCase("true")){
+            for (String instruction : instructions) {
+                System.out.println("inst -> "+instruction);
+                String error = TableCommande.executerCommande(instruction,false);
+                if (!error.equalsIgnoreCase("")){
+                    return "Une erreur est survenue lors du IF/ELSE : <br>" + instruction;
+
+                }
+            }
+        }
+        else if (argument.length == 5 && argument[3].equalsIgnoreCase("else")){
+            instructions = Convert.valeurStringtArgument(argument[4]);
+            for (String instruction : instructions) {
+                System.out.println("inst -> "+instruction);
+                String error = TableCommande.executerCommande(instruction,false);
+                if (!error.equalsIgnoreCase("")){
+                    return "Une erreur est survenue lors du IF/ELSE : <br>" + instruction;
+
+                }
+            }
+        }
 
 
 
