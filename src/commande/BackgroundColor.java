a9c72acd178cf4195692046f6732d711c3c49909
Adequin Renaud <adequin.renaud@gmail.com>
2014-01-05 00:38:33 +0100

    Ajout PENCOLOR et BGCOLOR

---
+++ src/commande/BackgroundColor.java
@@ -0,0 +1,7 @@
+package commande;
+
+/**
+ * Created by renaud on 05/01/14.
+ */
+public class BackgroundColor {
+}
---
+++ src/commande/Pencolor.java
@@ -0,0 +1,7 @@
+package commande;
+
+/**
+ * Created by renaud on 04/01/14.
+ */
+public class Pencolor {
+}
